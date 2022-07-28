14842c0c6be583bbf875316bc5d588b1ff66db74
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-07-28 15:00:11 +0000

    Add MyList class

--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,16 @@
 class MyList 
 include MyEnumerable
+  
+def initialize(*list)
+    @list = list
+  end
 
+def each(&block)
+    @list.each(&block)
 end
+end
+
+list = MyList.new(1, 2, 3, 4)
+
+list.all? { |e| e < 5}
+
