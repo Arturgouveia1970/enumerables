f338f51469717bf463dc9b4d831453f7974cc31d
arturgouveia1970 <artgouveiag@gmail.com>
2022-07-28 16:23:11 +0100

    added queries

--- my_list.rb
+++ my_list.rb
@@ -1,3 +1,5 @@
+require_relative './my_enumerables'
+
 class MyList 
 include MyEnumerable
   
@@ -12,5 +14,17 @@ end
 
 list = MyList.new(1, 2, 3, 4)
 
-list.all? { |e| e < 5}
+p(list.all? { |e| e < 5 })
+
+p(list.all? { |e| e > 5 })
+
+p(list.any? { |e| e == 2 })
+
+p(list.any? { |e| e == 5 })
+
+p(list.filter { |e| e.even? })
+
+
+
+
 
